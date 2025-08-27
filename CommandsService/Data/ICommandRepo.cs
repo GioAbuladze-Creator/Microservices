6e0c8c836825ea428963a7bcc982c7b261ca452d
GioAbuladze-Creator <gioabuladzeabu@gmail.com>
2025-08-27 16:10:41 +0400

    add commands

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,18 @@
+using CommandsService.Models;
+
+namespace CommandsService.Data;
+
+public interface ICommandRepo
+{
+    bool SaveChanges();
+
+    // Platform
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform plat);
+    bool PlatformExists(int platformId);
+
+    // Command
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command? GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+}
--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,9 +1,20 @@
+using CommandsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("InMem");
+});
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+
 
 var app = builder.Build();
 
