b05d3b88b4c067c51e94be655bec28b5c5d6301e
GioAbuladze-Creator <gioabuladzeabu@gmail.com>
2025-09-03 15:20:27 +0400

    add EventProcessingService

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,4 +1,5 @@
 using CommandsService.Data;
+using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("InMem");
 });
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 
 
 var app = builder.Build();
